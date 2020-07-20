9441592d04881540e792c9a772428726f198255d
madriko <mnurhafzi@gmail.com>
2020-07-20 10:26:09 +0700

    Menyalin isi folder build

---
+++ precache-manifest.2f82e280298176dfc9e6e8fd57e541ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "282b3b00fa0ad37802e7",
+    "url": "/static/css/main.f6242352.chunk.css"
+  },
+  {
+    "revision": "282b3b00fa0ad37802e7",
+    "url": "/static/js/main.282b3b00.chunk.js"
+  },
+  {
+    "revision": "2cb6552ed88dff81f03a",
+    "url": "/static/js/1.2cb6552e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c8bce623aac6242d827488daa0503128",
+    "url": "/index.html"
+  }
+];
