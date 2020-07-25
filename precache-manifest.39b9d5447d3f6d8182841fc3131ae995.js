93974be15be93baea8ef5319f1388a26444109c3
madriko <mnurhafzi@gmail.com>
2020-07-25 08:54:15 +0700

    menyalin food paradise react

---
+++ precache-manifest.39b9d5447d3f6d8182841fc3131ae995.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2171f984a9a06644ddf5e842b200c472",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc8bcdb024fd38c86f7d",
+    "url": "/static/css/main.5ad92253.chunk.css"
+  },
+  {
+    "revision": "ba2c7c2970f402f3240a",
+    "url": "/static/js/2.4aab50a5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4aab50a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc8bcdb024fd38c86f7d",
+    "url": "/static/js/main.8a312e94.chunk.js"
+  },
+  {
+    "revision": "28648d4b86d330626cee",
+    "url": "/static/js/runtime-main.fafa68b7.js"
+  },
+  {
+    "revision": "87c1b79904ac8dd73e0ce0d1dc3913c2",
+    "url": "/static/media/aneka-makanan.87c1b799.jpg"
+  }
+]);
